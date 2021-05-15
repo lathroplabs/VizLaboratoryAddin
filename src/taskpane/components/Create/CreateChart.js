0288b3953bc419d75d06e0dd422b14905674162d
Ken <gamingsciences@gmail.com>
2021-05-15 12:35:41 -0600

    Testing plotly responsive

--- src/taskpane/components/Create/CreateChart.js
+++ src/taskpane/components/Create/CreateChart.js
@@ -5,7 +5,7 @@ export default function CreateChartButton() {
   return (
     <div className="centered">
       <Label>Create Chart </Label>
-      <PrimaryButton text="Create" onClick={testPlotly} allowDisabledFocus />
+      <PrimaryButton text="Create" onClick={plotlyResponsive} allowDisabledFocus />
     </div>
   );
 }
@@ -32,4 +32,29 @@ function testPlotly() {
   } catch (error) {
     console.log('Error', error)
   }
+}
+
+function plotlyResponsive() {
+  var trace1 = {
+    type: 'bar',
+    x: [1, 2, 3, 4],
+    y: [5, 10, 2, 8],
+    marker: {
+        color: '#C8A2C8',
+        line: {
+            width: 2.5
+        }
+    }
+  };
+  
+  var data = [ trace1 ];
+  
+  var layout = { 
+    title: 'Responsive to window\'s size!',
+    font: {size: 18}
+  };
+  
+  var config = {responsive: true}
+  
+  Plotly.newPlot('tester', data, layout, config );
 }
