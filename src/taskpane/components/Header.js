876330de7bb4f7813e5785b69c97d57af69a88cc
lathroplabs <ken@predictionlaboratory.com>
2021-08-25 14:17:52 -0600

    Clean up header

--- src/taskpane/components/Header.js
+++ src/taskpane/components/Header.js
@@ -5,9 +5,8 @@ export default class Header extends React.Component {
     const { title, logo, message } = this.props;
 
     return (
-      <section className="ms-welcome__header ms-bgColor-neutralLighter ms-u-fadeIn500">
+      <section className="ms-welcome__header ms-u-fadeIn500">
         <img width="90" height="90" src={logo} alt={title} title={title} />
-        <h1 className="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">{message}</h1>
       </section>
     );
   }
